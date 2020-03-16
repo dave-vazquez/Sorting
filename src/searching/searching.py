50c0c4f0baa48ab192129b479a92c04eba3297e4
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-16 17:37:01 -0400

    iterative binary_search solution (done in lecture)

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -17,8 +17,14 @@ def binary_search(arr, target):
     low = 0
     high = len(arr)-1
 
-    # TO-DO: add missing code
-
+    while low <= high:
+        middle = (low + high) // 2
+        if target < arr[middle]:
+            high = middle - 1  # eliminate RHS
+        elif target > arr[middle]:
+            low = middle + 1  # eliminate LHS
+        else:
+            return middle
     return -1  # not found
 
 
