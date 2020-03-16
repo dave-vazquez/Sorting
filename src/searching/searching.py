9fa7e97bc863369d2cb7fdf9a418cfd0cd8757dc
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-16 17:54:11 -0400

    adds comments

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -34,13 +34,25 @@ def binary_search(arr, target):
 
 # STRETCH: write a recursive implementation of Binary Search
 def binary_search_recursive(arr, target, low, high):
+    # find the middle
     middle = (low+high) // 2
-
+    # base case: array is no length
     if len(arr) == 0:
         return -1
+    # recursive case: target is less than the middle element
     if target < arr[middle]:
-        return binary_search_recursive(arr, target, low, middle - 1)
+        # adjust high so it's one index behind the middle
+        high = middle - 1
+        # recursive binary search: return statement required to
+        # pass result down the call stack
+        return binary_search_recursive(arr, target, low, high)
     elif target > arr[middle]:
-        return binary_search_recursive(arr, target, middle + 1, high)
-
+        # adjust low so it's one index ahead of the middle
+        low = middle + 1
+        # recursive binary search: return statement required to
+        # pass result down the call stack
+        return binary_search_recursive(arr, target, low, high)
+    # returns middle index if target is neither lower or
+    # higher than the middle element...
+    # in otherwords, they're equal
     return middle
